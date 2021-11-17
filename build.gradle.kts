8c8bd91b6125ddba38560b59f9423fc2b5dad022
tuguzT <timurka.tugushev@gmail.com>
2021-11-17 21:29:40 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+@file:Suppress("GradlePackageUpdate")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.5.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.jpa") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
+}
+
+group = "io.github.tuguzt.sql.backend"
+version = "0.1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+}
+
+dependencies {
+    // Domain layer
+    implementation("com.github.tuguzT:sql_practice_5_domain:main-SNAPSHOT")
+
+    // Kotlin
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Kotlin extensions
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Third-Party
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+
+    // Testing
+    testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "SQLPractice5SpringBackend"
